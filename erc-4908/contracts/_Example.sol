8611b87d02b59793c4693f803d2a87346c453888
xdmaury <maury1418@hotmail.com>
2025-06-13 15:50:13 -0300

    feat: implement ERC4908 contract and interface for gated NFT access

---
+++ erc-4908/contracts/_Example.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {ERC4908} from "./ERC4908.sol";
+
+contract Example is ERC4908 {
+    constructor()
+        ERC4908("Gated resources NFT access ERC-4908 standard", "ERC4908")
+    {}
+}
